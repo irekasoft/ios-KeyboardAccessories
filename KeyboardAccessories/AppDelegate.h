860c0756ffce8b628d96300dcf7f63a8f9cffe2b
Muhammad Hijazi <irekasoft@gmail.com>
2016-05-07 02:32:10 +0800

    Initial Commit

---
+++ KeyboardAccessories/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyboardAccessories
+//
+//  Created by Hijazi on 7/5/16.
+//  Copyright © 2016 iReka Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeyboardAccessories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyboardAccessories
+//
+//  Created by Hijazi on 7/5/16.
+//  Copyright © 2016 iReka Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
